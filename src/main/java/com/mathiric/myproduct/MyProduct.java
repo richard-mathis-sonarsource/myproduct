00b3436d351d14ed8e06b018e28f73197feb773c
Richard Mathis <richard.mathis@sonarsource.com>
2021-05-27 11:50:24 +0200

    add

---
+++ src/main/java/com/mathiric/myproduct/MyProduct.java
@@ -0,0 +1,23 @@
+package com.mathiric.myproduct;
+
+public class MyProduct {
+
+    public static void main(String[] args) {
+        Rspec1656 test1656 = new Rspec1656();
+
+        test1656.rspec1656();
+
+        //rspec1656();
+
+    }
+
+    /*public static void rspec1656() {
+        String myString = "5";
+        Integer[] intArray = {1,2};
+
+        myString = myString;
+        intArray = intArray;
+    }*/
+
+
+}
